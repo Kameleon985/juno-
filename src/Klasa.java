e81c9f717794167d5413e91065fe16ee79200f0a
Kameleon985 <63420879+Kameleon985@users.noreply.github.com>
2020-04-09 20:49:07 +0200

    Update Klasa.java

--- src/Klasa.java
+++ src/Klasa.java
@@ -3,6 +3,11 @@ public class Klasa {
 
  public static void main (String[] args]) {
 	 System.out.println("xd");
+
+	 for(int i = 0;i<2;i++){
+	     if(i==2)
+	         System.out.println("JP2GMP");
+     }
  }
 	
 }
